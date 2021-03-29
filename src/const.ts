cf08a098ea61977200862175264cf5b722317550
dylandoamaral <do.amaral.dylan@gmail.com>
2021-03-29 22:41:48 +0200

    Prepare release v0.0.3

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/camelcase */
-export const CARD_VERSION = '0.0.2';
+export const CARD_VERSION = '0.0.3';
 
 export const DEFAULT_CONFIG = {
     hours_to_show: 24,
