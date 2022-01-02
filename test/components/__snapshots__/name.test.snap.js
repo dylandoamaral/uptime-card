094a4bf16ffe0ca4647907f62fffc39b928eccce
dylandoamaral <do.amaral.dylan@gmail.com>
2022-01-02 23:06:36 +0100

    test: add snapshot testing

---
+++ test/components/__snapshots__/name.test.snap.js
@@ -0,0 +1,10 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["uptime-card-name should return Unknown by default"] = 
+`<h1 style="">
+  Unknown
+</h1>
+`;
+/* end snapshot uptime-card-name should return Unknown by default */
+
--- test/components/name.test.ts
+++ test/components/name.test.ts
@@ -1,8 +1,15 @@
-import { expect, fixture, html } from '@open-wc/testing';
+import { UptimeCardName } from '../../src/components/name';
+
+import { expect, fixture } from '@open-wc/testing';
+import { html } from 'lit/static-html.js';
 
 describe('uptime-card-name', () => {
+  it('is defined', () => {
+    const el = document.createElement('uptime-card-name');
+    expect(el).instanceOf(UptimeCardName);
+  });
   it('should return Unknown by default', async () => {
     const el = await fixture(html`<uptime-card-name></uptime-card-name>`);
-    expect(el).dom.to.equalSnapshot();
+    await expect(el).shadowDom.to.equalSnapshot();
   });
 });
