4a867a3552f7834deaa00818f7c25b6ee21f21a2
dylandoamaral <do.amaral.dylan@gmail.com>
2022-01-03 20:54:08 +0100

    test: add name snapshots

--- test/components/__snapshots__/name.test.snap.js
+++ test/components/__snapshots__/name.test.snap.js
@@ -8,3 +8,24 @@ snapshots["uptime-card-name should return Unknown by default"] =
 `;
 /* end snapshot uptime-card-name should return Unknown by default */
 
+snapshots["uptime-card-name should return the configuration text if it exists"] = 
+`<h1 style="">
+  Test
+</h1>
+`;
+/* end snapshot uptime-card-name should return the configuration text if it exists */
+
+snapshots["uptime-card-name should return the sensor name if it exists and there is not configuration text"] = 
+`<h1 style="">
+  Sensor
+</h1>
+`;
+/* end snapshot uptime-card-name should return the sensor name if it exists and there is not configuration text */
+
+snapshots["uptime-card-name should apply translator correctly"] = 
+`<h1 style="">
+  Inconnue
+</h1>
+`;
+/* end snapshot uptime-card-name should apply translator correctly */
+
--- test/components/name.test.ts
+++ test/components/name.test.ts
@@ -2,14 +2,57 @@ import { UptimeCardName } from '../../src/components/name';
 
 import { expect, fixture } from '@open-wc/testing';
 import { html } from 'lit/static-html.js';
+import { ConfigurationName } from '../../src/types/configuration';
+import { getTranslator } from '../../src/utils/translator';
+
+const defaultConfiguration = {
+  show: true,
+  adaptative_color: false,
+};
 
 describe('uptime-card-name', () => {
   it('is defined', () => {
     const el = document.createElement('uptime-card-name');
     expect(el).instanceOf(UptimeCardName);
   });
+
   it('should return Unknown by default', async () => {
-    const el = await fixture(html`<uptime-card-name></uptime-card-name>`);
+    const el = await fixture(html`<uptime-card-name />`);
+    await expect(el).shadowDom.to.equalSnapshot();
+  });
+
+  it('should return the configuration text if it exists', async () => {
+    const configuration: ConfigurationName = {
+      text: 'Test',
+      ...defaultConfiguration,
+    };
+    const el = await fixture(html`<uptime-card-name .config=${configuration} />`);
+    await expect(el).shadowDom.to.equalSnapshot();
+  });
+
+  it('should return nothing if configuration show is false', async () => {
+    const configuration: ConfigurationName = {
+      ...defaultConfiguration,
+      show: false,
+    };
+    const el = await fixture(html` <uptime-card-name .config=${configuration} />`);
+    await expect(el).shadowDom.to.equalSnapshot();
+  });
+
+  it('should return the sensor name if it exists and there is not configuration text', async () => {
+    const el = await fixture(
+      html` <uptime-card-name
+        .config=${defaultConfiguration}
+        .sensorName=${'Sensor'}
+      />`,
+    );
+    await expect(el).shadowDom.to.equalSnapshot();
+  });
+
+  it('should apply translator correctly', async () => {
+    const el = await fixture(
+      html`<uptime-card-name .translator=${getTranslator('fr')} />`,
+    );
     await expect(el).shadowDom.to.equalSnapshot();
   });
 });
