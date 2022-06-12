b414c270c18faee0a5c52ca853b9b2d393971f52
dylandoamaral <do.amaral.dylan@gmail.com>
2022-06-12 17:40:06 +0200

    chore(release): prepare for v0.11.1

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/camelcase */
-export const CARD_VERSION = '0.11.0';
+export const CARD_VERSION = '0.11.1';
 
 export const DEFAULT_CONFIG = {
   hours_to_show: 24,
